be047a16b731418a0c0ed7b741cc9511e5ba1668
autoking777 <100059446+autoking777@users.noreply.github.com>
2022-10-23 00:43:36 -0500

    Update backend.tf

--- src/backend.tf
+++ src/backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-king7"
+    bucket         = "terraform-kinge7"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
 
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-king77-locks"
+    dynamodb_table = "terraform-king778-locks"
     encrypt        = true
   }
 }
